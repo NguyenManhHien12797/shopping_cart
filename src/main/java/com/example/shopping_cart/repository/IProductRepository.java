4a2766f5c436634d6c5ffcc12eb70c57367869bc
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-09-26 10:08:00 +0700

    first commit

---
+++ src/main/java/com/example/shopping_cart/repository/IProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.shopping_cart.repository;
+
+import com.example.shopping_cart.model.Product;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IProductRepository extends PagingAndSortingRepository<Product, Long> {
+
+
+}
